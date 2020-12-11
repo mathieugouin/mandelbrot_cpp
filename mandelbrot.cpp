b5fa0632c7514fed7c5b200cb593c1f87269637b
Mathieu Gouin <mgouin@gmail.com>
2020-12-11 18:32:19 -0500

    Update mandelbrot.cpp

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -26,9 +26,11 @@ int main () {
 
     if (my_Image.is_open()) {
         my_Image << "P3\n" << width << " " << height << " 255\n";
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++)  {
-                int val = value((double)i / width - 1.5, (double)j / height - 0.5);
+        for (int j = 0; j < height; j++)  {
+            for (int i = 0; i < width; i++) {
+                double x = (double)i / width * 2.0 - 0.75;
+                double y = (double)j / height * 2.0 - 1.0;
+                int val = value(x, y);
                 my_Image << val << ' ' << 0 << ' ' << 0 << "\n";
             }
         }
